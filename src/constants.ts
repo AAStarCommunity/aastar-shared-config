eb1a5da8518ece2f6a61cbe5df0d8b49124f0884
jhfnetboy <jhfnetboy@gmail.com>
2025-12-09 11:32:49 +0700

    feat: add V3 role constants

--- src/constants.ts
+++ src/constants.ts
@@ -55,3 +55,13 @@ export const NODE_STAKE_AMOUNTS = {
  * Default aPNTs price in USD (0.02 USD per aPNT)
  */
 export const DEFAULT_APNTS_PRICE_USD = "0.02";
+
+/**
+ * Registry V3 Role Identifiers (Keccak256)
+ */
+export const ROLE_ENDUSER = "0x0c34ecc75d3bf122e0609d2576e167f53fb42429262ce8c9b33cab91ff670e3a";
+export const ROLE_COMMUNITY = "0xe94d78b6d8fb99b2c21131eb4552924a60f564d8515a3cc90ef300fc9735c074";
+export const ROLE_PAYMASTER_AOA = "0xf86d13979a91272f2c54c4147e588ecbeaad32dd1e0a5332a77356847049a70e";
+export const ROLE_PAYMASTER_SUPER = "0x2024516755f401845808190bb830b0e4523d0df0845503cab52130a794699a0f";
+export const ROLE_KMS = "0x6fb2b25080d21c908f32ff8ff99eb1d552c210854f7de48a40406764c09c68f8";
+export const ROLE_ANODE = "0x3aa2e9509f29532b71f822e18bcde843a8070a508d564188ec8a6adf59eff28a";
